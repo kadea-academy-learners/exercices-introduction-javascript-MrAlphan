f431ceae57ee5d7b2e5ec2e532ae20fb3c3f108c
alphan wina wina <alphanisungu@gmail.com>
2025-09-25 07:25:09 +0100

    ajout des fonctionnalités dans l'exercice 4

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,7 @@
 // Exercice 5
-// Écrivez votre code ici
+
+let temperatureEnCelsius = 25;
+let temperatureEnFahrenheit = (temperatureEnCelsius * 9/5) + 32;
+
+console.log(temperatureEnCelsius + "°C équivaut à " + temperatureEnFahrenheit + "°F");
+
